10ac269d246b44d9e909c51bb3a793cce867413a
Alejandro Villarreal <m.ale.villarreal.0803@gmail.com>
2023-02-19 15:12:39 -0300

    cambios que no ocuparemos

--- js/index.js
+++ js/index.js
@@ -1,83 +1,28 @@
 
 
 // Funcion para buscar en la cartilla
-const dataBase= async()=>{
+const dataBase = async () => {
   let dataBase;
-  await fetch('http://localhost:3000/employees',{
-    method:"GET"
+  await fetch('http://localhost:3000/employees', {
+    method: "GET"
   })
-  .then((r)=>r.json())
-  .then((resp)=>dataBase=resp)
-  .catch((error)=>console.error(error))
+    .then((r) => r.json())
+    .then((resp) => dataBase = resp)
+    .catch((error) => console.error(error))
 
-  return dataBase.then(resp=>resp)
+  return dataBase.then(resp => resp)
 }
 
 
 ///////////
 //Constsntes utilizadas
-const urlEmployees = "http://localhost:3000/employees";
+
 const urlPacientes = "http://localhost:3000/patients";
 const urlTurnos = "http://localhost:3000/turns";
 
 
 
 
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(() => {
-  'use strict'
-
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  const forms = document.querySelectorAll('.needs-validation')
-
-  // Loop over them and prevent submission
-  Array.from(forms).forEach(form => {
-    form.addEventListener('submit', event => {
-      if (!form.checkValidity()) {
-        event.preventDefault()
-        event.stopPropagation()
-        // registrarMedicos()
-        
-      }else {
-        registrarMedicos();
-        alert("registro exitoso")
-        
-      }
-
-      form.classList.add('was-validated')
-    }, false)
-  })
-})()
-
-
-const registrarMedicos = () => {
-  //Cargos la data de form a las variables
-  const name = document.getElementById("nombre").value;
-  const lastName = document.getElementById("apellido").value;
-  const specialty = document.getElementById("especialidad").value;
-  const mail = document.getElementById("correo").value;
-  const pass = document.getElementById("password").value;
-  const authorized=false;
-  
-  //falta chequear mail si ya se encuentra registrado
-
-
-  //creo la esttuctura de datos que pasare al Json
-  const medico = { name, lastName, specialty, mail, pass , authorized};
-
-  fetch(urlEmployees, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(medico),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      alert("Usuario registrado exitosamente");
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
-};
 
 //Data Medicos por especialidad
 const obtenerMedicosPorEspecialidad = (Especialidad) => { };
@@ -96,7 +41,7 @@ const obtenerDataMedicoPorId = (id) => {
       // Código para procesar la lista de médicos obtenida
 
       const medicosFiltrados = data.filter(medico => medico.id === id);
-      console.log(medicosFiltrados);
+      // console.log(medicosFiltrados);
     })
     .catch((error) => {
       console.error("Error al realizar la solicitud:", error);
@@ -115,82 +60,31 @@ const obtenerDataMedicoPorEspecialidad = (especialidad) => {
       // Código para procesar la lista de médicos obtenida
 
       const medicosFiltrados = data.filter(medico => medico.specialty === especialidad);
-      console.log(medicosFiltrados);
+      //console.log(medicosFiltrados);
     })
     .catch((error) => {
       console.error("Error al realizar la solicitud:", error);
     });
 };
 
-const busquedaMail = (mail) => {
-  fetch(urlEmployees)
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
+
+const testMail = () => {
+
+  let dirMail = "JORGE.SUAREZ@GMAIL.COM";
+  //let dirMail = "";
+  buscarMedicoByMail(dirMail)
+    .then(encontrado => {
+      if (encontrado) {
+        console.log('El médico fue encontrado');
       } else {
-        throw new Error("Error al obtener la lista de médicos");
+        console.log('El médico no fue encontrado');
       }
-    })
-    .then((data) => {
-      // Código para procesar la lista de médicos obtenida
-
-      const medicosFiltrados = data.find(medico => medico.mail === mail);
-      if(medicosFiltrados){
-        
-        //retorna true por que encontro coincidencia del mail en la base de datos
-        return true
-      }else{
-        return false
-      }
-    })
-    .catch((error) => {
-      console.error("Error al realizar la solicitud:", error);
     });
 };
 
-const getUsuarios = () => {
-  fetch("usuarios.json")
-    .then((response) => response.json())
-    .then((data) => {
-      const usuarios = data.usuarios;
-      console.log(usuarios);
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
-};
-
-// Obtener db
-
-const getUser = () => {
-  const urlUsers = "http://localhost:3000/medicos";
-
-  fetch(urlUsers)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Error al obtener los datoss de los usuarios");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-
-  //fetch(dbUrl)
-  // .then((Response) =>  {
-  //   return Response.json();
-  // })
-  // .then((json) => {
-  //   return json;
-  // })
-};
 
 // Cargar el archivo JSON con los turnos
-
-function getTurnos() {
+const getTurnos = () => {
   // Definir la fecha deseada (en este ejemplo, buscamos los turnos del 1 de marzo de 2023)
   let fechaBuscada = "2023-03-01";
 
@@ -202,4 +96,100 @@ function getTurnos() {
   // Mostrar los turnos filtrados
   console.log(turnosFiltrados);
 }
+
+// Busqueda en Employees por mail
+//Usar como testMail()
+const buscarMedicoByMail = async (mail) => {
+  try {
+    const response = await fetch(urlEmployees);
+    if (!response.ok) {
+      throw new Error('Error al obtener la lista de médicos');
+    }
+    const medicos = await response.json();
+    console.log(medicos)
+    const encontrado = medicos.some(medico => medico.mail === mail);
+
+    return encontrado;
+  } catch (error) {
+    console.error('Error al realizar la solicitud:', error);
+  }
+};
+
+const buscarMedicoByMail2 = (mail) => {
+  fetch(urlEmployees)
+    .then((response) => {
+      response.json();
+    })
+    .then((data) => {
+      const medicos = data.employees;
+      const encontrado = medicos.some(medico => medico.mail === mail);
+      console.log(encontrado);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+};
+
+function buscarMedicoPorMail(mail) {
+  fetch(urlEmployees)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Error al obtener la lista de médicos');
+      }
+      return response.json();
+    })
+    .then(medicos => {
+      const encontrado = medicos.some(medico => medico.mail === mail);
+      return encontrado;
+      // if (encontrado) {
+      //   console.log('El médico fue encontrado');
+      // } else {
+      //   console.log('El médico no fue encontrado');
+      // }
+    })
+    .catch(error => {
+      console.error('Error al realizar la solicitud:', error);
+    });
+}
+// funcion para busacar por patrametros
+function buscarPorValor(url, NameProp, findValue) {
+  fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Error al obtener la lista de médicos');
+      }
+      return response.json();
+    })
+    .then(data => {
+
+      const encontrado = data.some(res => `res.${NameProp}` === findValue);
+      //return encontrado;
+      if (encontrado) {
+        console.log('El valor indicado fue encontrado',);
+      } else {
+        console.warn('El valor indicado no fue encontrado');
+      }
+    })
+    .catch(error => {
+      console.error('Error al realizar la solicitud:', error);
+    });
+}
+
+const test2 = () => {
+  //buscarPorValor(urlEmployees, "mail", "");
+  //buscarPorValor(urlEmployees, "mail", "YOLANDA.RODRIGUEZ@GMAIL.COM");
+  buscarPorValor(urlEmployees, "name", "YOLANDA");
+}
+//Ejemplo de busqueda
+const getUsuarios = () => {
+  fetch("usuarios.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const usuarios = data.usuarios;
+      console.log(usuarios);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+};
 
---
+++ js/regMed.js
@@ -0,0 +1,86 @@
+//Constantes utilizadas
+const urlEmployees = "http://localhost:3000/employees";
+
+const addEmployees = () => {
+
+  //Cargos la data de form a las variables
+  const name = document.getElementById("nombre").value;
+  const lastName = document.getElementById("apellido").value;
+  const specialty = document.getElementById("especialidad").value;
+  const mail = document.getElementById("correo").value;
+  const pass = document.getElementById("password").value;
+  const authorized = false;
+
+  //creo la estructura de datos que pasare al Json
+  const nuevoMedico = { name, lastName, specialty, mail, pass, authorized };
+
+  registrarMedico(nuevoMedico);
+}
+
+
+const registrarMedico = async (medico) => {
+  try {
+    const response = await fetch(urlEmployees);
+    if (!response.ok) {
+      throw new Error('Error al obtener la lista de médicos');
+    }
+    const medicos = await response.json();
+    if (medicos.some(m => m.mail === medico.mail)) {
+      console.log('Error: el médico ya existe en la base de datos');
+      return;
+    }
+    //medicos.push(medico);
+    const postResponse = await fetch(urlEmployees, {
+      method: 'POST',
+      body: JSON.stringify(medico),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    if (!postResponse.ok) {
+      throw new Error('Error al guardar los datos del médico');
+    }
+    console.log('El médico ha sido registrado exitosamente');
+  } catch (error) {
+    console.error('Error al realizar la solicitud:', error);
+  }
+};
+
+const buscarMedico = async (mail, nombre) => {
+  try {
+    const response = await fetch(urlEmployees);
+    if (!response.ok) {
+      throw new Error('Error al obtener la lista de médicos');
+    }
+    const medicos = await response.json();
+    return medicos.some(medico => medico.mail === mail && medico.name === nombre);
+  } catch (error) {
+    console.error('Error al realizar la solicitud:', error);
+  }
+};
+
+const buscarYMostrarMedico = async (mail, nombre) => {
+  try {
+    const encontrado = await buscarMedico(mail, nombre);
+    if (encontrado) {
+      console.log('El médico fue encontrado ', nombre, mail);
+    } else {
+      console.log('El médico no fue encontrado', nombre, mail);
+    }
+  } catch (error) {
+    console.error('Error al buscar el médico:', error);
+  }
+};
+
+const nMedico = {
+  nombre: 'Alejandro',
+  mail: 'ale@ale.com'
+};
+const nMedico2 = {
+  nombre: 'EDUARDO',
+  mail: 'EDUARDO.COMOTTI@GMAIL.COM'
+};
+
+//registrarMedico(nuevoMedico);
+//buscarYMostrarMedico(nMedico.mail, nMedico.nombre);
+//buscarYMostrarMedico(nMedico2.mail, nMedico2.nombre);
